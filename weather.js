ec61b62020beecf4df1f8fffdca430dbecdc88c8
Ashley Stewart <Ashley Stewart>
2017-01-08 23:27:18 +1300

    Geolocation API implimentation

--- weather.js
+++ weather.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4ff7bb103cde6a536bccae16f8a995f", function (json) {
-            $(".message").html(JSON.stringify(json));        
+    function getUserLocation() {
+        $.get('http://ip-api.com/json', function (locData) {
+            $('.city').text(locData.city + ", " + locData.region + ", " + locData.country);
+        }
+        )
+    }
+
+
+
+
+
+
+
+
+
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4ff7bb103cde6a536bccae16f8a995f", function (json) {
+        $(".message").html(JSON.stringify(json));
     });
+
+    var x = {}
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    // function showPosition(position) {
+    //     x.innerHTML = "Latitude: " + position.coords.latitude +
+    //         "<br>Longitude: " + position.coords.longitude;
+    // }
 });

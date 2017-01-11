d7101c1e9a8040f7f11b8c31f7e664cfb95167aa
Ashley Stewart <Ashley Stewart>
2017-01-11 23:28:26 +1300

    API working now with local users JSON data returning

--- weather.js
+++ weather.js
@@ -1,35 +1,6 @@
 $(document).ready(function () {
-    function getUserLocation() {
-        $.getJSON('http://ip-api.com/json', function (locData) {
-            console.writeline("hello" + locData)
-            $('.city').html(locData.city + ", " + locData.region + ", " + locData.country);
-        }
-        )
-    }
-
-
-
-
-
-
-
-
-
-
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4ff7bb103cde6a536bccae16f8a995f", function (json) {
+    $.getJSON("http://ip-api.com/json", function (json) {
         $(".message").html(JSON.stringify(json));
-    });
-
-    var x = {}
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
-    // function showPosition(position) {
-    //     x.innerHTML = "Latitude: " + position.coords.latitude +
-    //         "<br>Longitude: " + position.coords.longitude;
-    // }
+    });    
 });
+

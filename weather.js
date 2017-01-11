91bf8b3d05460213596bc727dc529cb69e0150b4
Ashley Stewart <Ashley Stewart>
2017-01-11 23:46:00 +1300

    heading displaying country

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $.getJSON("http://ip-api.com/json", function (json) {
         $(".message").html(JSON.stringify(json));
+        $(".countryHeading").html(JSON.stringify(json.country))
     });    
 });
 
